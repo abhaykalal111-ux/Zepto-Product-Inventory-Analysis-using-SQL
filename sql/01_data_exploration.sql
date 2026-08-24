6ef8a7ab5f96fd5eba68ad4c622756c156ffb736
abhaykalal111-ux <abhaykalal111@gmail.com>
2026-08-25 02:58:52 +0530

    Update 01_data_exploration.sql

--- sql/01_data_exploration.sql
+++ sql/01_data_exploration.sql
@@ -1,1 +1,34 @@
+USE zepto;
 
+-- ==========================================
+-- ZEPTO SQL BUSINESS ANALYSIS
+-- DATA EXPLORATION
+-- ==========================================
+
+-- 1. Total number of products
+SELECT COUNT(*) AS total_products
+FROM zepto_products;
+
+
+-- 2. View sample records
+SELECT *
+FROM zepto_products
+LIMIT 10;
+
+
+-- 3. View table structure
+DESCRIBE zepto_products;
+
+
+-- 4. Check categories
+SELECT DISTINCT Category
+FROM zepto_products;
+
+
+-- 5. Count products by category
+SELECT
+    Category,
+    COUNT(*) AS product_count
+FROM zepto_products
+GROUP BY Category
+ORDER BY product_count DESC;
